d9b5dd9052ccf95179bf01dadffa8a2b1117cfb6
Diego Pagini <dq@qbkconsulting.com>
2021-02-21 08:26:39 -0300

    push notificaations

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+// Enable CORS
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
+
 // Directorio Público
 app.use(express.static(publicPath));
 
